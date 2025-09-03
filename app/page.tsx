a37b750e77666189277e3190c8c114048fbe4648
yoseph0524 <yoseph0524@g.ucla.edu>
2025-09-03 01:07:20 -0700

    Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block ">
-          Email is required.
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium outline-none md:px-10 bg-black ">
-          Log in
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        <div className="group flex flex-col">
+          <input
+            className="bg-gray-100 w-full"
+            placeholder="Write your email"
+          />
+          <span className="group-focus-within:block hidden">
+            Make sure it is a valid email...
+          </span>
+          <button>Submit</button>
+        </div>
       </div>
     </main>
   );
